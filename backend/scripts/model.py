11ca70af8a9141a35e3f119301403f1af6f57a0d
Rachita Udupa <rachitaudupa@gmail.com>
2025-07-11 10:06:14 -0400

    model stuff

--- backend/scripts/model.py
+++ backend/scripts/model.py
@@ -10,7 +10,7 @@ with open('model.pkl', 'rb') as f:
 
 input_df = pd.DataFrame([{'jobRole': job_role, 'location': location}])
 input_X = pd.get_dummies(input_df)
-input_X = input_X.reindex(columns=feature_columns, fill_values=0)
+input_X = input_X.reindex(columns=feature_columns, fill_value=0)
 
 prediction = model.predict(input_X)[0]
 print(prediction)
